5e78fa9f0eed93343f53c07d2145ebcb93f5492e
Venkata rama krishna sai kumar Madiraju <1000054733@hexaware.com>
2022-07-13 12:23:27 +0530

    maven project

---
+++ RunTimeCheck/src/main/java/GenRocket/RunTimeCheck/App.java
@@ -0,0 +1,17 @@
+package GenRocket.RunTimeCheck;
+
+import com.genRocket.engine.EngineManual;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println( "Hello World!" );
+    	EngineManual engineManual = new EngineManual();
+    	System.out.println("hello again");
+    }
+}
